3b4fe298004ba4aeddd60c93154abeeb0a53d21c
Gonzalo <palmer_85@hotmail.com>
2020-04-11 01:08:14 -0500

    El resto de los ejercicios del modulo de basic algorithm

--- js/script.js
+++ js/script.js
@@ -57,21 +57,172 @@ txt.addEventListener('keyup',(event)=>{
     })
 
 
+///////////////////////////////////////////////////////////
+
+// 4.- Find the longest word in a string (encontrar la palabra mas largar en una cadena)
+
+
+function findLongestWordLength(str) {
+  let cadena = str.split(' '); // se divide el argunmento por palabras mediante la funcion split y usando un espacio separador
+   let cadenaN=[];
+
 /*
-function factorialize(num) {
-  let factNum = 1;
-
-  for(let i=1; i<=num; i++){
-    factNum= i * factNum;
-    console.log(factNum);
-  }
-  return factNum;
-}
-factorialize(5);
-
-//TODO:crear un div y colocar un texto al colocar el digito btener el factorial y 
-marcar si es meno de cero como error.
+Con el ciclo for se obtiene los tamaños de las palabras y se agregan en el nuevo arreglo
 */
+ for(let i=0; i < cadena.length; i++){ 
+    cadenaN.push((cadena[i].length));
+ }
+  return (Math.max(...cadenaN)); // la función Math.max() regresa el valor más alto  de un arreglo
+}
+
+
+///////////////////////////////////////////////////
+
+ //return largest numbers in arrays
+ 
+ 
+  function largestOfFour(arr) {
+  let newArr =[]; // se crea elñ nuevo arreglo que contendra los valores más alto del arreglo original
+  for(let i = 0; i <arr.length; i++){ // se recorre cada 
+     newArr.push((Math.max.apply(null,arr[i])));  // una variante del math.max  determianr el mayor de cada arreglo y lo agregar al nuevo arreglo
+  }
+  return newArr;
+}
+
+
+
+   ////////////////////////////////////////////////////
+  //confirm the ending
+  
+function confirmEnding(str, target) {
+
+return str.slice(str.length - target.length) == target;
+ 
+}
+
+confirmEnding("Bastian", "n");
+  
+
+////////////////////////////////////////////////////////
+//Repeat a string
+
+function repeatStringNumTimes(str, num) {
+  let cadena =[];
+  for(let i=0; i<num; i++){
+     cadena.push(str);
+  }
+  return (cadena.join(''));
+}
+
+repeatStringNumTimes("*", 3);
+
+
+////////////////////////////////////////////////////////
+
+//truncate a string
+
+function truncateString(str, num) {
+
+if(str.length > num){
+  return (str.substring(0,num)+'...');
+  } else {
+    return str;
+  }
+}
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+
+///////////////////////////////////////////////////////////
+
+//Finders Keepers
+
+function findElement(arr, func) {
+  for (let i = 0; i < arr.length; i++){
+    if(arr[i] % 2 === 0){
+      return (arr[i]);
+    }
+  }
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
+
+//////////////////////////////////////////////////////////
+
+//Boo who
+
+function booWho(bool) {
+  return typeof bool ==='boolean';
+}
+
+booWho(true);
+
+///////////////////////////////////////////////////////////
+
+//Title Case a Sentence
+
+function titleCase(str) {
+  let newCadena=[]
+  let cadena = str.toLowerCase().split(' ');
+  for( let i in cadena){
+    newCadena[i] = cadena[i].replace(cadena[i].charAt(0),cadena[i].charAt(0).toUpperCase());
+    
+  }
+  return newCadena.join(' ');
+}
+
+titleCase("I'm a little tea pot");
+
+///////////////////////////////////////////////////////////
+
+//slice and splice
+
+function frankenSplice(arr1, arr2, n) {
+let nArray = arr2.slice();
+nArray.splice(n, 0, ...arr1);
+return nArray;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+//////////////////////////////////////////////////////////
+
+//Falsy Bounce
+
+function bouncer(arr) {
+  
+ return arr.filter(Boolean);
+}
+
+bouncer([7, "ate", "", false, 9]);
+ 
+
+//////////////////////////////////////////////////////////
+// Mutation
+
+function mutation(arr) {
+  var test = arr[1].toLowerCase();
+  var target = arr[0].toLowerCase();
+  for (var i = 0; i < test.length; i++) {
+    if (target.indexOf(test[i]) < 0) return false;
+  }
+  return true;
+}
+///////////////////////////////////////////////////////////
+
+//Chunkey Monkey
+
+function chunkArrayInGroups(arr, size) {
+  // Break it up.
+  var arr2 = [];
+  for (var i = 0; i < arr.length; i += size) {
+    arr2.push(arr.slice(i, i + size));
+  }
+  return arr2;
+}
+
+
+
+ 
 
 
 
